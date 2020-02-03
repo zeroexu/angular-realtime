5fa522348ddec52e0fbfa491720d8d85dd722eff
Ali Lopez <ali.lopez.gonzalez@igam.io>
2020-02-03 12:21:20 -0300

    add userProfile component
     ng g c UserProfile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserProfileListComponent } from './user-profile-list/user-profile-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileListComponent
+    UserProfileListComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule
